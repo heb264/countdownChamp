2861c0cffc7f528b45cdcd0ac3fa455639babd24
Elizabeth H Burgess <heb264@gmail.com>
2017-10-08 13:52:15 +0800

    update after completing tutorial.

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Clock from './Clock.js';
+import './App.css';
+import {Form, FormControl, Button} from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    };
+  }
+
+  changeDeadline(){
+    this.setState({deadline: this.state.newDeadline}); //never change state directly use setState
+  }
+
+  render(){
+  return(
+    <div className="App">
+    <div className="App-title">Countdown to {this.state.deadline}</div>
+    <Clock
+      deadline={this.state.deadline}
+    />
+      <Form inline>
+      <FormControl
+        className="Deadline-input"
+        placeholder='new date'
+        onChange={event => this.setState({newDeadline: event.target.value})} />
+        <button onClick={() => this.changeDeadline()}>Submit</button>
+      </Form>
+    </div>
+  )
+  }
+}
+
+export default App;
